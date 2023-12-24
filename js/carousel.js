2a508953dec7678b075ad5e97debd7bd20d4cc80
ShenghaoisYummy <hsupisces@hotmail.com>
2023-12-24 13:11:33 +0800

    points for carousel completed

--- js/carousel.js
+++ js/carousel.js
@@ -1,50 +1,70 @@
 var carousel = document.getElementById('carousel_list');
 var leftbtn = document.getElementById('left_btn');
 var rightbtn = document.getElementById('right_btn');
+var points = document.getElementById('points');
+
+var circle_li = points.getElementsByTagName('li');
 
 var idx = 0;
+var lock = false;
 
-rightbtn.onclick = function () {
+rightbtn.onclick = right_btn_handler;
+leftbtn.onclick = left_btn_handler;
 
-    carousel.style.transition = 'transform .5s ease 0s'
+
+function right_btn_handler() {
+
+    if (lock == true) {
+        return;
+    }
+
+    lock = true;
+
+    carousel.style.transition = 'transform .5s ease 0s';
 
     idx++;
     carousel.style.transform = `translateX(${ -16.66 * idx }%)`;
 
     if (idx > 4) {
-
+        idx = 0;
+        setCurrent();
         setTimeout(() => {
 
             carousel.style.transition = 'none';
             carousel.style.transform = 'none';
-            idx = 0;
-
         }, 500);
+
     }
 
+    setCurrent();
+
+    setTimeout(() => {
+        lock = false;
+    }, 500)
 };
 
-leftbtn.onclick = function () {
+function left_btn_handler() {
 
+    if (lock == true) {
+        return;
+    }
+
+    lock = true;
 
     if (idx == 0) {
-
+        idx = 5;
         // to picture 5 instantly
         carousel.style.transition = 'none';
-        carousel.style.transform = `translateX(${ -16.66 * 5 }%)`;
+        carousel.style.transform = `translateX(${ -16.66 * idx }%)`;
 
-
+        idx = 4
+        setCurrent()
         setTimeout(() => {
 
-            setTimeout(() => {
+            carousel.style.transition = 'transform .5s ease 0s'
+            carousel.style.transform = `translateX(${ -16.66 * idx }%)`;
 
-                idx = 4
-                carousel.style.transition = 'transform .5s ease 0s'
-                carousel.style.transform = `translateX(${ -16.66 * idx }%)`;
-
-            }, 0);
-        }, 500);
-
+        }, 0);
 
     } else {
 
@@ -52,4 +72,42 @@ leftbtn.onclick = function () {
         carousel.style.transform = `translateX(${ -16.66 * idx }%)`;
 
     }
-};
+
+    setCurrent()
+
+    setTimeout(() => {
+        lock = false;
+    }, 500)
+}
+
+function setCurrent() {
+
+    for (var i = 0; i < 5; i++) {
+
+        if (i == idx) {
+
+            circle_li[i].className = 'current';
+
+        } else {
+
+            circle_li[i].className = '';
+
+        }
+    }
+
+}
+
+points.onclick = function (e) {
+
+    if (e.target.tagName.toLowerCase() == 'li') {
+
+        var n = e.target.getAttribute('data-n');
+    }
+    idx = n;
+    carousel.style.transition = 'transform .5s ease 0s';
+    carousel.style.transform = `translateX(${ -16.66 * idx }%)`;
+    setCurrent();
+}
+
+
+setInterval(right_btn_handler,2500);
